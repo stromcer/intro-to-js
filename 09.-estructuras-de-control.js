6d14028092313a120b812e06f93e2b2e441ef200
agarzon48 <garzonximenez@gmail.com>
2023-01-27 17:06:48 +0000

    Ready to second class

--- 09.-estructuras-de-control.js
+++ 09.-estructuras-de-control.js
@@ -51,6 +51,18 @@ const oneToHundredArray = Array.from({length: 100}, (_, i) => i + 1)
 //     console.log(i);
 // }
 
+// EJERCICIOS
+
+// 1. Crea un contador que imprima en pantalla
+// los números naturales del 1 al 100
+
+// 2. Crea un contador que imprima en pantalla
+// los números naturales impares del 1 al 100
+
+// 3. Modifica tu anterior contador para que
+// imprima los números naturales impares del
+// 100 al 1
+
 // for in
 
 // const sampleObject = { a: 1, b: 2, c: 3 };
@@ -59,11 +71,38 @@ const oneToHundredArray = Array.from({length: 100}, (_, i) => i + 1)
 //   console.log(property);
 // }
 
+// EJERCICIO
+
+// 1. Itera sobre oneToHundredArray para imprimir en
+// pantalla una cuenta de 2 en 2 desde 2 hasta 200
+
+// 2. Ahora crea un FizzBuzz
+
 // for of
 
 // for (const number of oneToHundredArray) {
 //     console.log(number);
 // }
 
+// EJERCICIO
+
+// 1. Imprime todas las propiedades y valores de
+// sampleObject. Ejemplo: "Propiedad a: valor 1, ..."
+
 // while
 // do while
+
+// let currentNumber = 0;
+
+// while (currentNumber < 100) {
+//   currentNumber++;
+// }
+
+// console.log(currentNumber);
+
+// let currentNumber = 0;
+
+// do {
+//     console.log(currentNumber);
+//     currentNumber++;
+// } while (currentNumber < 0);
